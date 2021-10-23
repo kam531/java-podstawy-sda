2accce8b5b44fe724407b6064af2e3d55ca1d5a1
Kamil <kamiltokarskifot@gmail.com>
2021-10-23 11:36:53 +0200

    RozwiązanieZadania1

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
+        //1.Napisz program, który pobierze od użytkownika średnicę okręgu
+        // (zmienną typu float) i obliczy obwód tego okręgu. Do obliczeń przyjmij najpierw π = 3.14,
+        // a następnie skorzystaj z wbudowanej klasy Math i znajdującej się tam stałej PI.
 
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Podaj średnicę okręgu,żeby obliczyć jego obwód");
+        float diamter = scanner.nextFloat();
+        final double Pi1 = 3.14;
+        double PI = Math.PI;
+        float circumferenceofacircle= (float) (diamter * PI);
+        System.out.println("Obwodkoła wynosi " + circumferenceofacircle);
     }
 }
